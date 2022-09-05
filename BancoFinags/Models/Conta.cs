3b476caa20a973b406dfe3ce422902c0b86311e8
Lucas Gomes <lukinhas082@hotmail.com>
2022-09-05 08:14:17 -0300

    Add project files.

---
+++ BancoFinags/Models/Conta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoFinags.Models
+{
+    public class Conta
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }
+        public double Saldo { get; set; }
+    }
+}
