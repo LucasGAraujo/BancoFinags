ec00c26f865d2517a441e76b288d1cfb6126af92
Lucas Gomes <lukinhas082@hotmail.com>
2022-09-12 09:17:48 -0300

    salve fazer em casa

--- BancoFinags/Program.cs
+++ BancoFinags/Program.cs
@@ -12,8 +12,33 @@ namespace BancoFinags
             List<Conta> contas = new List<Conta>();
 
             CriarContas(contas);
+            IncluirConta(contas);
             MostrarContas(contas);
         }
+        public static void IncluirConta(List<Conta> contas)
+        {
+            Console.Write("Entre com o Numero da Conta: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Entre com o Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Entre com o Saldo: ");
+            double saldo = Convert.ToDouble(Console.ReadLine());
+            contas.Add(new Conta(num, nome, saldo));
+        }
+        public static void AlterarConta()
+        {
+
+        }
+        public static void ExcluirConta()
+        { 
+
+        }
+        public static void ExibirConta()
+        {
+
+        }
+
+        
         public static void CriarContas(List<Conta> contas) 
         {
             Conta conta = new Conta(1, "Lucas", 2000);
